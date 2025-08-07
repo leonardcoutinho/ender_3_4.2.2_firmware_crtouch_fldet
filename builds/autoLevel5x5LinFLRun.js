a4ed7267c27dca5ccaac4a53e750e09bd2374355
Leonardo Coutinho <leonardcoutinho@gmail.com>
2025-08-07 12:33:51 -0300

    Update autoLevel5x5LinFLRun.js

--- builds/autoLevel5x5LinFLRun.js
+++ builds/autoLevel5x5LinFLRun.js
@@ -1,9 +1,9 @@
 module.exports = {
     extends: "builds/baseLin.js",
-    include: {
+    include: [
         "builds/features/autoLevel.js",
         "builds/features/fl_detect.js"
-    },
+    ],
     meta: {
         stable_name: "ender_3_4.2.2-{{marlin_version}}-auto_level_5x5_lin-{{uid}}",
         nightly_name: "ender_3_4.2.2-{{current_date}}-auto_level_5x5_lin-{{uid}}"
