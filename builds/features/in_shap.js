b13d2480507804ecac5c03a38dd74481fd2394b1
Leonardo Coutinho <leonardcoutinho@gmail.com>
2025-08-07 12:58:54 -0300

    Update in_shap.js

--- builds/features/in_shap.js
+++ builds/features/in_shap.js
@@ -1,7 +1,7 @@
 module.exports = {
     partial: true,
     configuration_adv: {
-        enable: [
+        enable: [ //enable input shapping function on marlin
             "INPUT_SHAPING_X",
             "INPUT_SHAPING_Y",
             "SHAPING_MENU"
