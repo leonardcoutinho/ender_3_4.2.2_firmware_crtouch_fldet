444b3cce0649cbb3fe64064cd41575d0f568e8ce
Leonardo Coutinho <leonardcoutinho@gmail.com>
2025-08-07 20:11:39 -0300

    Create anetA8_linadv_fldet_level_IS.js

---
+++ builds/anetA8_linadv_fldet_level_IS.js
@@ -0,0 +1,21 @@
+module.exports = {
+    extends: "builds/default_anetA8.js",
+    meta: {
+        stable_name: "anet_A8_4.2.2-{{marlin_version}}-base-{{uid}}",
+        nightly_name: "anet_A8_4.2.2-{{current_date}}-base-{{uid}}"
+    },
+    configuration: {
+        enable: [
+            ["STRING_CONFIG_H_AUTHOR", "(Dust, valerionew, Ender-3, leonardcoutinho)"],
+            //standard leveling menu helper
+            "LCD_BED_TRAMMING",
+            "BED_TRAMMING_INCLUDE_CENTER"
+        ]
+    },
+    configuration_adv: {
+        enable: [
+            //octoprint
+            "HOST_ACTION_COMMANDS"
+        ]
+    }
+};
